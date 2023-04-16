e3368a770da4cfbd7275c1f4977f0a3ca6c4e840
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-16 23:29:49 +0600

    dart array practicew

---
+++ array.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // array as list
+  List array = [1, 2, 3, 4, 'sakil'];
+
+  print(array);
+  print(array[4]);
+
+  // object literal as map
+
+  Map object = {'name': 'sakil', 'email': 'sakil@gmail.com'};
+
+  print(object);
+  print(object['email']);
+}
--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
 void main() {
   print('sakil');
+
+  /// doc comment a-z A-A 0-9 _ $ variable names
+  /// Data types
+  /// int
+  /// Double
+  /// String
+  /// bool
+  /// List
+  /// Map
+  /// Dynamic
+  ///
+  bool bools = true;
+  print(bools);
 }
