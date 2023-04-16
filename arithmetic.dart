c69904cc975e3151d05a7ce7aef4ad9267bc8e23
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-16 23:50:26 +0600

    dart arithmetic operators

---
+++ arithmetic.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var num1 = 10;
+  var num2 = 2;
+
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 * num2);
+  print(num1 % num2);
+  print(num1 / num2);
+  print(num1 ~/ num2);
+}
---
+++ dynamic_var.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+void main() {
+  var dynamic = "sakil";
+
+  print(dynamic);
+
+  var f_name = "sakil";
+
+  var l_name = "jomadder";
+
+  var full_name = (f_name + " " + l_name).toUpperCase();
+  print(full_name);
+}
