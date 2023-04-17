1a39eb8b90a013c994258443fc4ffdafafc58f50
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-17 12:57:12 +0600

    dart string practice

---
+++ string.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  // String name = "sakil";
+
+  // print(name);
+
+  // print(name.runtimeType);
+
+  // String name2 = "rasel";
+
+  // print(name2);
+
+  // var name3 = "monir";
+
+  // print(name3);
+
+  // print(name3.runtimeType);
+
+  var multi = '''this is 
+  multiline string''';
+
+  print(multi);
+  print(multi.runtimeType);
+}
