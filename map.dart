653bd1bc74743cc5cba76f217600a78de78beaf0
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-21 14:57:39 +0600

    dynamic empty map

--- map.dart
+++ map.dart
@@ -1,5 +1,14 @@
 void main(List<String> args) {
   /// map literal
 
-  var map_literal = {'name': 'sakil'};
+  // var map_literal = {'name': 'sakil'};
+
+  /// empty map
+  /// dynamic empty map
+
+  // var empty_map = {};
+
+  // print(empty_map);
+
+  // print(empty_map.runtimeType);
 }
