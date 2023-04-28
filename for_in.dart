239b5c4bd44c9561ffd90c24e87d5f0dd5b0db81
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-28 22:58:45 +0600

    for in loop on list and set

--- for_in.dart
+++ for_in.dart
@@ -1,7 +1,13 @@
 void main(List<String> args) {
-  var set1 = ['sakil', 'rasel', 'monir'];
+  var person_list = ['sakil', 'rasel', 'monir'];
 
-  for (var item in set1) {
+  for (var item in person_list) {
+    print(item);
+  }
+
+  var fruits_set = {'apple', 'orange', 'mango'};
+
+  for (var item in fruits_set) {
     print(item);
   }
 }
