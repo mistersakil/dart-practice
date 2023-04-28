340f4a49d5fb5d9381ca38d73a61bcd86ae38c72
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-28 23:05:28 +0600

    for in loop on map

--- for_in.dart
+++ for_in.dart
@@ -1,13 +1,27 @@
 void main(List<String> args) {
-  var person_list = ['sakil', 'rasel', 'monir'];
+  // var person_list = ['sakil', 'rasel', 'monir'];
 
-  for (var item in person_list) {
-    print(item);
-  }
+  // for (var item in person_list) {
+  //   print(item);
+  // }
 
-  var fruits_set = {'apple', 'orange', 'mango'};
+  // var fruits_set = {'apple', 'orange', 'mango'};
 
-  for (var item in fruits_set) {
-    print(item);
-  }
+  // for (var item in fruits_set) {
+  //   print(item);
+  // }
+
+  // Map<String, dynamic> person_map = {
+  //   'name': 'sakil',
+  //   'email': 'sakil@gmail.com',
+  //   'age': 32
+  // };
+
+  // for (var key in person_map.keys) {
+  //   print("${key} => ${person_map[key]}");
+  // }
+
+  // for (var value in person_map.values) {
+  //   print(value);
+  // }
 }
