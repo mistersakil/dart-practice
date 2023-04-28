f038cce31e570c9c94323ed80273656553f32d67
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-28 23:38:20 +0600

    function with named parameters

--- function.dart
+++ function.dart
@@ -1,20 +1,27 @@
 void main(List<String> args) {
   /// working on function
-  sum(10, 5);
-  anno(10, 3);
-  calculation(sum);
+  // sum(10, 5);
+  // anno(10, 3);
+  // calculation(sum);
+  /// function with return value
+
+  // int sum(int a, int b) {
+  //   return a + b;
+  // }
+
+  // int calculation(Function callback) {
+  //   return callback();
+  // }
+
+  /// arrow function
+
+  // int anno(int a, int b) => a + b;
+
+  /// named parameters
+
+  // named({a, b}) {
+  //   print(a + b);
+  // }
+
+  // named(b: 10, a: 20);
 }
-
-/// function with return value
-
-int sum(int a, int b) {
-  return a + b;
-}
-
-int calculation(Function callback) {
-  return callback();
-}
-
-/// arrow function
-
-int anno(int a, int b) => a + b;
