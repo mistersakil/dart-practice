299607c13cf3503e6fcdffb2a15cb54032b241ed
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-30 11:48:25 +0600

    handle exception using try catch block and using on keyword

---
+++ exception.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  /// handle exception using try catch block
+  try {
+    var result = 4 % 0;
+    print(result);
+  } catch (e) {
+    if (e.runtimeType == IntegerDivisionByZeroException) {
+      print('Divided by zero not allowed');
+    }
+  }
+
+  /// handle exception using on keyword
+  // try {
+  //   var result = 4 % 0;
+  //   print(result);
+  // } on IntegerDivisionByZeroException {
+  //   print('Divided by zero not allowed');
+  // }
+}
