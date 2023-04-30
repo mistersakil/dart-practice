abb43f80d008a1ec049db09f7b845b204aa8de94
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-04-30 12:07:32 +0600

    importing librery file

--- exception.dart
+++ exception.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main(List<String> args) {
   /// handle exception using try catch block
   // try {
@@ -18,14 +20,21 @@ void main(List<String> args) {
   // }
 
   /// handle exception using try catch block
-  try {
-    var result = 4 % 0;
-    print(result);
-  } catch (e) {
-    if (e.runtimeType == IntegerDivisionByZeroException) {
-      print('Divided by zero not allowed');
-    }
-  } finally {
-    print('always run');
-  }
+  // try {
+  //   var result = 4 % 0;
+  //   print(result);
+  // } catch (e) {
+  //   if (e.runtimeType == IntegerDivisionByZeroException) {
+  //     print('Divided by zero not allowed');
+  //   }
+  // } finally {
+  //   print('always run');
+  // }
+
+  // try {
+  //   var root = sqrt(16);
+  //   print(root);
+  // } catch (e) {
+  //   print(e);
+  // }
 }
